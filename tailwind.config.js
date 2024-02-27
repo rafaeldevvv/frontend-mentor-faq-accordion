ea39d1bce671b63b12a82f4bbe30cc513bc531a6
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2024-02-27 16:15:38 -0300

    feat: style page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
       white: "hsl(0, 0%, 100%)",
       lightpink: "hsl(275, 100%, 97%)",
       grayishpurple: "hsl(292, 16%, 49%)",
-      darkpurple: "hsl(292, 42%, 14%)"
+      darkpurple: "hsl(292, 42%, 14%)",
+      gray: "hsl(295 5% 51%)"
     },
     fontFamily: {
       sans: ["WorkSans", ...defaultTheme.fontFamily.sans]
     },
-    extend: {},
   },
   plugins: [],
 }
