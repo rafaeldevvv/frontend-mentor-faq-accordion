985ad2bb0779d488bb35f484a404119d23210e58
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2024-02-27 13:47:28 -0300

    chore: customize tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      lightpink: "hsl(275, 100%, 97%)",
+      grayishpurple: "hsl(292, 16%, 49%)",
+      darkpurple: "hsl(292, 42%, 14%)"
+    },
     extend: {},
   },
   plugins: [],
